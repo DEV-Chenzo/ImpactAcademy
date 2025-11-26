8cb37e395d889570fe3c4de988d5504e13c250ed
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2025-11-26 19:29:52 -0300

    test color gold

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
       <div className="container">
         <div className="hero-inner">
           <h1>
-            O Futuro da Educação Digital <span>Começa Aqui!!!</span>
+            O Futuro da Educação Digital <span className='color-gold' >Começa Aqui!!!</span>
           </h1>
 
           <div className="cta" role="navigation" aria-label="Chamada para ação">
@@ -39,7 +39,7 @@ export default function Hero() {
               SAIBA MAIS!
             </button>
             <button className="btn btn-outline" id="btn-conheca" onClick={handleConheca}>
-              CONHEÇA A EMPRESA!
+              <span className="color-gold">CONHEÇA A EMPRESA!</span>
             </button>
           </div>
         </div>
